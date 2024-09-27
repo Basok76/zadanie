d092a5f0505c50d19f655f569d8e47dabdf6d40f
Basok76 <puntsukov35@gmail.com>
2024-09-27 22:35:02 +0800

    Выполненное задание

---
+++ sort-app/backend/server.js
@@ -0,0 +1,57 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const { Pool } = require('pg');
+const cors = require('cors');
+
+const app = express();
+const port = 5000;
+
+const pool = new Pool({
+    user: 'postgres',
+    host: 'localhost',
+    database: 'web_postgres',
+    password: '123asd',
+    port: 5432,
+});
+
+app.use(cors());
+app.use(bodyParser.json());
+
+app.post('/save-sorted', async (req, res) => {
+    const numbers = req.body.numbers;
+    try {
+        const client = await pool.connect();
+        const result = await client.query('INSERT INTO sorted_arrays DEFAULT VALUES RETURNING id');
+        const arrayId = result.rows[0].id;
+        await Promise.all(numbers.map(async (number) => {
+            await client.query('INSERT INTO sorted_elements (array_id, element) VALUES ($1, $2)', [arrayId, number]);
+        }));
+        client.release();
+    } catch (err) {
+        console.error(err);
+    }
+});
+
+app.get('/get-sorted/:id', async (req, res) => {
+    const arrayId = req.params.id; 
+    try {
+        const client = await pool.connect();
+        const result = await client.query('SELECT element FROM sorted_elements WHERE array_id = $1 ORDER BY id', [arrayId]);
+        client.release();
+        if (result.rows.length > 0) {
+            const elements = result.rows.map(row => row.element); 
+            res.status(200).json(elements); 
+        } else {
+            res.status(404).send("");
+        }
+    } catch (err) {
+        console.error(err);
+    }
+});
+
+app.listen(port, () => {
+    console.log(`Сервер запущен: http://localhost:${port}`);
+});
+
+// C:\Program Files\PostgreSQL\17\bin
+
---
+++ sort-app/src/App.js
@@ -0,0 +1,82 @@
+import logo from './logo.svg';
+import './App.css';
+import React, { useState } from 'react';
+
+function App() {
+  const [inputValue, setInputValue] = useState('');
+  const [sortedArray, setSortedArray] = useState([]);
+  const [inputIdentif, setIdentifValue] = useState('');
+  const [sortedOutArray, setSortedOutArray ] = useState([]);
+  const bubbleSort = (arr) => {
+    let n = arr.length;
+    for (let i = 0; i < n - 1; i++) {
+        for (let j = 0; j < n - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+            
+                [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
+            }
+        }
+    }
+    return arr;
+  };
+
+  const inSortedArray = async (arr) => {
+    const a = arr.split(',').map(Number); 
+     if (a.some(isNaN)) {
+        alert('Ошибка: введите только числа, разделенные запятыми.');
+        return; 
+    }
+    const sorted = bubbleSort(a);
+    setSortedArray(sorted);
+    try {
+      const response = await fetch('http://localhost:5000/save-sorted', {
+          method: 'POST',
+          headers: {
+              'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({ numbers: sorted }),
+        });
+        if (!response.ok) {
+        }
+      } catch (error) {
+      console.error(error);
+    }
+  }
+
+  const outSortedArray = async () => {
+    try {
+        const response = await fetch(`http://localhost:5000/get-sorted/${inputIdentif}`);
+        const data = await response.json();
+        setSortedOutArray(data);
+    } catch (error) {
+        alert("Данные введены неверно");
+    }
+};
+
+  return(  
+      <div>
+        <h1>Пузырковая сортировка</h1>
+        <input
+            type="text"
+            value={inputValue}
+            onChange={(e) => setInputValue(e.target.value)}
+            placeholder="Введите числа через запятую"
+        />
+        <button onClick={() => inSortedArray(inputValue)}>Сортировать</button>
+        <h2>Результат сортировки:</h2>
+        <p>{sortedArray.join(', ')}</p>
+        <h1>идентификатор сортировки:</h1>
+        <input
+            type="text"
+            value={inputIdentif}
+            onChange={(e) => setIdentifValue(e.target.value)}
+            placeholder="Введите идентификатор массива"
+        />
+          <button onClick={outSortedArray}>Получить массив из БД</button>
+        <h2>Массив из БД:</h2>
+        <p>{sortedOutArray.join(', ')}</p>
+    </div>
+  );
+}
+
+export default App;
